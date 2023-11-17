9e92146ffcaa4131a7c1f627c8318985ccbb89ea
Raphael Freire <raphaelfreire@Raphaels-MacBook-Pro.local>
2023-11-17 10:47:29 -0300

    upload repository github

---
+++ src/admin/app.tsx
@@ -0,0 +1,18 @@
+import Icon from "./extensions/icon.png";
+import Logo from "./extensions/logo.svg";
+
+export default {
+  config: {
+    auth: {
+      logo: Logo,
+    },
+    head: {
+      favicon: Icon,
+    },
+    locales: [],
+    menu: {
+      logo: Icon,
+    },
+  },
+  bootstrap() {},
+};
